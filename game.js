af1c5134793426dbc9b2015982de422d7a2179ca
lisa22np <erli22np@student.ju.se>
2024-11-13 17:50:24 +0100

    still working on start screen

--- game.js
+++ game.js
@@ -2,6 +2,12 @@ function setup() {
   createCanvas(800, 600);
 }
 
+let cowX = 400;
+let cowY = 10;
+let cowS = 0.5;
+
+let stateOfGame = 0;
+
 function cow(x, y, s) {
   noStroke();
 
@@ -151,45 +157,111 @@ function cow(x, y, s) {
   endShape();
 }
 
-let cowX = 400;
-let cowY = 50;
-let cowS = 0.5;
+function gameBackground() {
+  background(17, 18, 28);
+  fill(15, 33, 19);
+  ellipse(600, 500, 700, 450);
+
+  fill(21, 46, 26);
+  ellipse(200, 500, 600, 400);
+
+  noStroke();
+  fill(28, 69, 26);
+  rect(0, 540, 800, 100);
+
+  beginShape();
+  vertex(180, 550);
+  vertex(175, 515);
+  vertex(190, 530);
+  vertex(210, 505);
+  vertex(205, 550);
+  endShape();
+
+  beginShape();
+  vertex(595, 550);
+  vertex(585, 525);
+  vertex(600, 530);
+  vertex(610, 510);
+  vertex(615, 530);
+  vertex(630, 525);
+  vertex(620, 550);
+  endShape();
+
+  fill(36, 255, 0, 80);
+  beginShape();
+  vertex(395, 0);
+  vertex(260, 540);
+  vertex(540, 540);
+  vertex(405, 0);
+  endShape();
+  arc(400, 540, 280, 20, 0, PI, CHORD);
+}
 
 function startScreen() {
-  background(10, 10, 10);
-
   push();
   fill(43, 189, 10);
   strokeWeight(10);
   stroke(21, 92, 4);
-  rect(250, 400, 200, 100);
+  rect(300, 380, 200, 100);
   pop();
 
   push();
   fill(21, 92, 4);
   textSize(50);
-  text("START", 269, 465);
+  text("START", 320, 445);
   pop();
 
   push();
-  let helpText = "help";
-  fill(255, 255, 255);
+  const helpText1 = "We need your help! This cow we abducted,";
+  const helpText2 = "Betsy, is proving very hard to return. Could";
+  const helpText3 = "you help us land her safetly back on earth?";
+  const helpText4 = "Use your spacebar to slow down her decent.";
+  const helpText5 = "We'd be ever so gratefull!";
+  fill(196, 250, 187);
   textSize(15);
-  text(helpText, 250, 100);
+  text(helpText1, 50, 100);
+  text(helpText2, 50, 120);
+  text(helpText3, 50, 140);
+  text(helpText4, 50, 160);
+  text(helpText5, 50, 180);
   pop();
 }
 
 function gameScreen() {
-  background(10, 10, 10);
-
   cow(cowX, cowY, cowS);
-  if (cowY < 500) {
-    cowY = cowY * 1.04;
+  if (cowY < 430) {
+    cowY = cowY * 1.08;
   }
 }
 
-function endScreen() {}
+function endScreenWin() {
+  fill(255, 255, 255);
+  text("You win!", 200, 200);
+}
+
+function endScreenLose() {
+  fill(255, 255, 255);
+  text("You fail!", 200, 200);
+}
 
 function draw() {
-  startScreen();
+  if (stateOfGame === 0) {
+    gameBackground();
+    startScreen();
+  } else if (stateOfGame === 1) {
+    gameBackground();
+    gameScreen();
+  } else if (stateOfGame === 2) {
+    gameBackground();
+    endScreenWin();
+  } else if (stateOfGame === 3) {
+    gameBackground();
+    endScreenLose();
+  }
 }
+
+//function mouseClicked(){
+  //if (stateOfGame === 0){
+    //stateOfGame = 1;
+ // }
+//}
